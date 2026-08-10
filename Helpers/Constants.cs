01c22d9cebeda80da7c8378ea710da961cb14997
lungavazi <lungabhara@gmail.com>
2026-08-10 02:44:07 +0200

    Add delete confirmation message.

--- Helpers/Constants.cs
+++ Helpers/Constants.cs
@@ -3,7 +3,7 @@
     public static class Constants
 
     {
-        public const string API_URL = "https://localhost:7112/api/v1/";
+        public const string API_URL = "http://localhost:5205/api/v1/";
 
         public const string LastStatusText = "Last life-status sync: Not yet checked";
     }
